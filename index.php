7e1f43d806208ae8e8cc99f13f383dca06f94f69
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2023-05-24 10:11:32 +0700

    Models

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
 
+/**
+ * Generate session uuid
+ */
 $session_uuid = strtoupper(vsprintf('%s%s-%s-%s-%s-%s%s', str_split(bin2hex(random_bytes(16)), 4)));
 session_id($session_uuid);
 session_start();
 
+/**
+ * Define constants
+ */
 define("ROOT_DIR", __DIR__);
 define("CONTROLLER_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "controllers");
+define("MODEL_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "models");
 define("VIEW_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "views");
-define("LIB_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "libs");
+define("VENDORS_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "vendors");
+define("HELPERS_DIR", ROOT_DIR . DIRECTORY_SEPARATOR . "helpers");
 
-require_once "./helpers.php";
+/**
+ * Import needed files
+ */
+require_once "./helpers/helpers.php";
 require_once "./controllers/app.controller.php";
-require_once "./controllers/pdf.controller.php";
+require_once "./models/pdf.model.php";
 
+/**
+ * Launching application
+ */
 $app = new AppController();
 $app->run();
